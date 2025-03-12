323bc2494966cdbf82b0646fd9cf7c4bba9e9c4c
HopeMani17 <88143139+HopeMani17@users.noreply.github.com>
2025-03-12 13:14:22 +1100

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,84 @@ content.style.color="white";
 container.appendChild(content);
 
 // buttons is a node list. It looks and acts much like an array.
-const button=document.querySelectorAll("button");
+// const button=document.querySelectorAll("button");
+// const buttonOne = document.querySelector("#one");
+// buttonOne.addEventListener("click", gethumanSelection);
 
-const buttonOne = document.querySelector("#one");
-buttonOne.addEventListener("click", gethumanSelection);
 
-const buttonTwo = document.querySelector("#two");
-buttonTwo.addEventListener("click", getcomputerSelection);
+// const buttonTwo = document.querySelector("#two");
+// buttonTwo.addEventListener("click", getcomputerSelection);
+
 
 const buttonThree = document.querySelector("#three");
 buttonThree.addEventListener("click", playGame);
+
+
+
+
+//Function to play game
+  
+ function playGame()
+ {
+    let humanScore=0;
+    let computerScore=0;
+    const humanSelection = gethumanSelection();
+    const computerSelection = getcomputerSelection();
+    
+    if(computerSelection === humanSelection)
+        { 
+            const output = document.createElement("div");
+            output.classList.add("output");
+            output.textContent = "it is a tie. you both picked same item";
+            output.style.color="white";
+            container.appendChild(output);  
+        }
+        else if(humanSelection === "rock" && computerSelection === "scissors")
+        {
+            const output = document.createElement("div");
+            output.classList.add("output");
+            output.textContent = "You win! rock beats scissor";
+            output.style.color="white";
+            container.appendChild(output); 
+            humanScore++;
+        }
+        else if(humanSelection === "rock" && computerSelection === "paper")
+        { 
+            const output = document.createElement("div");
+            output.classList.add("output");
+            output.textContent = "You win! paper beats rock";
+            output.style.color="white";
+            container.appendChild(output); 
+            humanScore++;
+        }
+         else if(humanSelection === "scissors" && computerSelection === "paper")
+        {
+            const output = document.createElement("div");
+            output.classList.add("output");
+            output.textContent = "You win! scissors beats paper";
+            output.style.color="white"; container.appendChild(output); 
+            humanScore++;
+        }
+        else
+        {   const output = document.createElement("div");
+            output.classList.add("output");
+            output.textContent = "you lose! Computer selection beats you";
+            output.style.color="white"; 
+            container.appendChild(output); 
+            computerScore++;
+        }
+    
+        const output = document.createElement("div");
+        output.classList.add("output");
+        output.textContent = "Final score: " + humanScore + " to " + computerScore + "";
+        output.style.color="white"; 
+        container.appendChild(output);
+    
+ }
+
+//   for (let i = 1; i <= 5; i++) {
+//     playGame(i);
+//  }
 
 //Function to get value for computer
 function getcomputerSelection()
@@ -44,62 +112,3 @@ function gethumanSelection()
     let humanInput = prompt("Please enter your choice:(rock, paper, or scissors):");
     return humanInput;
 }
-const humanSelection = gethumanSelection();
-const computerSelection = getcomputerSelection();
-//Function to play game
-  
- function playGame()
- {
-    let humanScore=0;
-    let computerScore=0;
-    
-    if(computerSelection === humanSelection)
-        { 
-            const output = document.createElement("div");
-            output.classList.add("output");
-            output.textContent = "it is a tie. You both picked same item";
-            output.style.color="white";
-            container.appendChild(output);  
-        }
-        else if(humanSelection === "rock" && computerSelection === "scissors")
-        {
-            const output = document.createElement("div");
-            output.classList.add("output");
-            output.textContent = "You win! Rock beats Scissor";
-            output.style.color="white";
-            container.appendChild(output); 
-            humanScore++;
-        }
-        else if(humanSelection === "rock" && computerSelection === "paper")
-        { 
-            const output = document.createElement("div");
-            output.classList.add("output");
-            output.textContent = "You win! Paper beats Rock";
-            output.style.color="white";
-            container.appendChild(output); 
-            humanScore++;
-        }
-         else if(humanSelection === "scissors" && computerSelection === "paper")
-        {
-            const output = document.createElement("div");
-            output.classList.add("output");
-            output.textContent = "You win! Scissors beats Paper";
-            output.style.color="white"; container.appendChild(output); 
-            humanScore++;
-        }
-        else
-        {   const output = document.createElement("div");
-            output.classList.add("output");
-            output.textContent = "You lose! Computer selection beats you";
-            output.style.color="white"; 
-            container.appendChild(output); 
-            computerScore++;
-        }
-    
-        const output = document.createElement("div");
-        output.classList.add("output");
-        output.textContent = "Final score: " + humanScore + " to " + computerScore + "";
-        output.style.color="white"; 
-        container.appendChild(output);
-    
- }
